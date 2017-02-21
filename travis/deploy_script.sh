ab67afb8f76eb6a73469f58630add3862a1b73b3
Dat Nguyen <thanhdat@siliconstraits.com>
2017-02-21 22:45:21 +0700

    final setup for travis

--- travis/deploy_script.sh
+++ travis/deploy_script.sh
@@ -1,8 +1,1 @@
-curl
-    -F "status=2"
-    -F "notify=1"
-    -F "notes=Bugs fix and improvements."
-    -F "notes_type=0"
-    -F "ipa=@app/build/outputs/apk/app-release.apk"
-    -H "X-HockeyAppToken: $HOCKEYAPP_TOKEN"
-    https://rink.hockeyapp.net/api/2/apps/upload
+curl -F "status=2" -F "notify=1" -F "notes=Bugs fix and improvements." -F "notes_type=0" -F "ipa=@app/build/outputs/apk/app-release.apk" -H "X-HockeyAppToken: $HOCKEYAPP_TOKEN" https://rink.hockeyapp.net/api/2/apps/upload
